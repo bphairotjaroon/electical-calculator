2542da03efcc068c2d1a7533289669e5adb493a5
Phairot J <b.phairot.j@gmail.com>
2026-09-19 09:14:46 +0700

    Fix Render port configuration

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 const httpServer = http.createServer(app);
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
